ce4fe5d10da32a37088a8604feb2d0202cfb82be
kooritea <ochatokori@gmail.com>
2021-08-23 16:07:48 +0800

    添加通过创建/sdcard/disable_fcmfix来禁用模块

--- app/src/main/java/com/kooritea/fcmfix/XposedMain.java
+++ app/src/main/java/com/kooritea/fcmfix/XposedMain.java
@@ -3,6 +3,9 @@ package com.kooritea.fcmfix;
 import com.kooritea.fcmfix.xposed.BroadcastFix;
 import com.kooritea.fcmfix.xposed.MiuiLocalNotificationFix;
 import com.kooritea.fcmfix.xposed.ReconnectManagerFix;
+
+import java.io.File;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -10,6 +13,10 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XposedMain implements IXposedHookLoadPackage {
 
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if(fileIsExists("/sdcard/disable_fcmfix")){
+            XposedBridge.log("[fcmfix] /sdcard/disable_fcmfix is exists, exit");
+            return;
+        }
         if(loadPackageParam.packageName.equals("android")){
             XposedBridge.log("[fcmfix] start hook com.android.server.am.ActivityManagerService");
             new BroadcastFix(loadPackageParam);
@@ -17,10 +24,22 @@ public class XposedMain implements IXposedHookLoadPackage {
             XposedBridge.log("[fcmfix] start hook com.android.server.notification.NotificationManagerServiceInjector");
             new MiuiLocalNotificationFix(loadPackageParam);
         }
-        if(loadPackageParam.packageName.equals("com.google.android.gms")){
+        if(loadPackageParam.packageName.equals("com.google.android.gms") && loadPackageParam.isFirstApplication){
             XposedBridge.log("[fcmfix] start hook com.google.android.gms");
             new ReconnectManagerFix(loadPackageParam);
         }
 
+    }
+    private boolean fileIsExists(String strFile) {
+        try {
+            File f = new File(strFile);
+            if(!f.exists()) {
+                return false;
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
