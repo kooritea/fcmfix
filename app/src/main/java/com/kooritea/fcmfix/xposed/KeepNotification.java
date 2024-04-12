b5a0c2505db350eed081192e845c478e618c42e2
kooritea <ochatokori@gmail.com>
2024-04-12 22:29:01 +0800

    修复安卓14部分版本阻止应用停止时自动清除通知遗功能异常

--- app/src/main/java/com/kooritea/fcmfix/xposed/KeepNotification.java
+++ app/src/main/java/com/kooritea/fcmfix/xposed/KeepNotification.java
@@ -1,5 +1,6 @@
 package com.kooritea.fcmfix.xposed;
 
+import android.os.Build;
 import java.lang.reflect.Method;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -33,12 +34,46 @@ public class KeepNotification extends XposedModule{
             }
         }
         if(targetMethod != null){
-            int pkg_args_index = 2;
-            int reason_args_index = 8;
+            int pkg_args_index = 0;
+            int reason_args_index = 0;
+            if(Build.VERSION.SDK_INT == 30){
+                pkg_args_index = 2;
+                reason_args_index = 8;
+            }
+            if(Build.VERSION.SDK_INT == 31){
+                pkg_args_index = 2;
+                reason_args_index = 8;
+            }
+            if(Build.VERSION.SDK_INT == 32){
+                pkg_args_index = 2;
+                reason_args_index = 8;
+            }
+            if(Build.VERSION.SDK_INT == 33){
+                pkg_args_index = 2;
+                reason_args_index = 8;
+            }
+            if(Build.VERSION.SDK_INT == 34){
+                if(targetMethod.getParameterTypes().length == 10){
+                    pkg_args_index = 2;
+                    reason_args_index = 8;
+                }else if(targetMethod.getParameterTypes().length == 8){
+                    pkg_args_index = 2;
+                    reason_args_index = 7;
+                }
+            }
+            if(Build.VERSION.SDK_INT > 34){
+                pkg_args_index = 2;
+                reason_args_index = 7;
+            }
+            if(pkg_args_index == 0 || reason_args_index == 0){
+                throw new NoSuchMethodError();
+            }
+            int finalPkg_args_index = pkg_args_index;
+            int finalReason_args_index = reason_args_index;
             XposedBridge.hookMethod(targetMethod,new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) {
-                    if(isDisableAutoCleanNotification() && targetIsAllow((String) param.args[pkg_args_index]) && (int)param.args[reason_args_index] == 5){
+                    if(isDisableAutoCleanNotification() && targetIsAllow((String) param.args[finalPkg_args_index]) && (int)param.args[finalReason_args_index] == 5){
                         param.setResult(null);
                     }
                 }
