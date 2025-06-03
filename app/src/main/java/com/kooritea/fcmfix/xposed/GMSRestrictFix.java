67b79368b73642393e96044ec944e8657cf10e22
YuSaki丶Kanade <nep_timeline@outlook.com>
2025-06-03 16:26:57 +0800

    拦截isGoogleRestricInfoOn

--- app/src/main/java/com/kooritea/fcmfix/xposed/GMSRestrictFix.java
+++ app/src/main/java/com/kooritea/fcmfix/xposed/GMSRestrictFix.java
@@ -8,20 +8,25 @@ public class GMSRestrictFix extends XposedModule {
     public GMSRestrictFix(XC_LoadPackage.LoadPackageParam loadPackageParam) {
         super(loadPackageParam);
         try {
-            this.startHookRegisterGmsRestrictObserver();
+            this.startHookRegisterGmsRestrictObserver(); // 阻止Hans监听GMS状态更新
         } catch (Throwable e) {
             printLog("hook error registerGmsRestrictObserver:" + e.getMessage());
         }
         try {
-            this.startHookUpdateGmsRestrict();
+            this.startHookUpdateGmsRestrict(); // 拦截Hans更新GMS限制状态
         } catch (Throwable e) {
             printLog("hook error updateGmsRestrict:" + e.getMessage());
         }
         try {
-            this.startHookIsGmsApp();
+            this.startHookIsGoogleRestricInfoOn(); // 阻止判断GMS限制
+        } catch (Throwable e) {
+            printLog("hook error isGoogleRestricInfoOn:" + e.getMessage());
+        }
+        /*try {
+            this.startHookIsGmsApp(); // 阻止Hans对GMS进行特殊处理
         } catch (Throwable e) {
             printLog("hook error isGmsApp:" + e.getMessage());
-        }
+        }*/
     }
 
     private void startHookRegisterGmsRestrictObserver() {
@@ -30,6 +35,10 @@ public class GMSRestrictFix extends XposedModule {
 
     private void startHookUpdateGmsRestrict() {
         XposedHelpers.findAndHookMethod("com.android.server.hans.scene.OplusBgSceneManager", loadPackageParam.classLoader, "updateGmsRestrict", XC_MethodReplacement.DO_NOTHING);
+    }
+
+    private void startHookIsGoogleRestricInfoOn() {
+        XposedHelpers.findAndHookMethod("com.android.server.am.OplusAppStartupManager$OplusStartupStrategy", loadPackageParam.classLoader, "isGoogleRestricInfoOn", int.class, XC_MethodReplacement.returnConstant(false));
     }
 
     private void startHookIsGmsApp() {
