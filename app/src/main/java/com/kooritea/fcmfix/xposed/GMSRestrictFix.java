6a8e15b476da2e8ca7039c6f660725aa0e427185
YuSaki丶Kanade <nep_timeline@outlook.com>
2025-06-03 14:24:55 +0800

    优化GMSRestrictFix错误拦截

--- app/src/main/java/com/kooritea/fcmfix/xposed/GMSRestrictFix.java
+++ app/src/main/java/com/kooritea/fcmfix/xposed/GMSRestrictFix.java
@@ -8,17 +8,31 @@ public class GMSRestrictFix extends XposedModule {
     public GMSRestrictFix(XC_LoadPackage.LoadPackageParam loadPackageParam) {
         super(loadPackageParam);
         try {
-            this.startHookGMSRestrict();
+            this.startHookRegisterGmsRestrictObserver();
         } catch (Throwable e) {
             printLog("hook error registerGmsRestrictObserver:" + e.getMessage());
         }
+        try {
+            this.startHookUpdateGmsRestrict();
+        } catch (Throwable e) {
+            printLog("hook error updateGmsRestrict:" + e.getMessage());
+        }
+        try {
+            this.startHookIsGmsApp();
+        } catch (Throwable e) {
+            printLog("hook error isGmsApp:" + e.getMessage());
+        }
     }
 
-    private void startHookGMSRestrict() {
+    private void startHookRegisterGmsRestrictObserver() {
         XposedHelpers.findAndHookMethod("com.android.server.hans.scene.OplusBgSceneManager", loadPackageParam.classLoader, "registerGmsRestrictObserver", XC_MethodReplacement.DO_NOTHING);
+    }
 
+    private void startHookUpdateGmsRestrict() {
         XposedHelpers.findAndHookMethod("com.android.server.hans.scene.OplusBgSceneManager", loadPackageParam.classLoader, "updateGmsRestrict", XC_MethodReplacement.DO_NOTHING);
+    }
 
+    private void startHookIsGmsApp() {
         XposedHelpers.findAndHookMethod("com.android.server.hans.OplusHansDBConfig", loadPackageParam.classLoader, "isGmsApp", int.class, XC_MethodReplacement.returnConstant(false));
     }
 }
