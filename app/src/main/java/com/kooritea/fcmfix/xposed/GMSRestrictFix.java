68a216542866390234b023445556478f0d06f225
YuSaki丶Kanade <nep_timeline@outlook.com>
2025-06-03 14:18:48 +0800

    阻止系统对GMS进行特殊处理

--- app/src/main/java/com/kooritea/fcmfix/xposed/GMSRestrictFix.java
+++ app/src/main/java/com/kooritea/fcmfix/xposed/GMSRestrictFix.java
@@ -18,5 +18,7 @@ public class GMSRestrictFix extends XposedModule {
         XposedHelpers.findAndHookMethod("com.android.server.hans.scene.OplusBgSceneManager", loadPackageParam.classLoader, "registerGmsRestrictObserver", XC_MethodReplacement.DO_NOTHING);
 
         XposedHelpers.findAndHookMethod("com.android.server.hans.scene.OplusBgSceneManager", loadPackageParam.classLoader, "updateGmsRestrict", XC_MethodReplacement.DO_NOTHING);
+
+        XposedHelpers.findAndHookMethod("com.android.server.hans.OplusHansDBConfig", loadPackageParam.classLoader, "isGmsApp", int.class, XC_MethodReplacement.returnConstant(false));
     }
 }
